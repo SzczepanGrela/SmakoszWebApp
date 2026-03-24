87c771a311bbf6a704a206b8799fc7cdccc0b5de
SzczepanGrela <szczepangrela@wp.pl>
2026-03-24 02:01:25 +0100

    added missing JwtTokenService and PasswordHasher files

---
+++ src/Smakosz.Infrastructure/Services/PasswordHasher.cs
@@ -0,0 +1,16 @@
+using Smakosz.Application.Common.Interfaces;
+
+namespace Smakosz.Infrastructure.Services;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password, workFactor: 12);
+    }
+
+    public bool Verify(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
