40f3246996cd38aff7e18eb664915c8873d24221
SzczepanGrela <szczepangrela@wp.pl>
2026-03-26 13:36:01 +0100

    added bUnit test infrastructure

---
+++ tests/Smakosz.ClientTests/Common/BunitTestBase.cs
@@ -0,0 +1,37 @@
+using Blazored.LocalStorage;
+using Bunit.TestDoubles;
+
+namespace Smakosz.ClientTests.Common;
+
+public abstract class BunitTestBase : Bunit.TestContext
+{
+    protected TestAuthorizationContext AuthContext { get; }
+
+    protected BunitTestBase()
+    {
+        AuthContext = this.AddTestAuthorization();
+
+        Services.AddSingleton<ToastService>();
+
+        Services.AddSingleton(Substitute.For<IHomeService>());
+        Services.AddSingleton(Substitute.For<IDishService>());
+        Services.AddSingleton(Substitute.For<IReviewService>());
+        Services.AddSingleton(Substitute.For<IUserProfileService>());
+        Services.AddSingleton(Substitute.For<ISearchService>());
+        Services.AddSingleton(Substitute.For<INotificationService>());
+        Services.AddSingleton(Substitute.For<IMediaService>());
+        Services.AddSingleton(Substitute.For<IBusinessService>());
+        Services.AddSingleton(Substitute.For<IAdminService>());
+        Services.AddSingleton(Substitute.For<IAuthService>());
+        Services.AddSingleton(Substitute.For<IContentService>());
+        Services.AddSingleton(Substitute.For<IRecommendationService>());
+        Services.AddSingleton(Substitute.For<IRestaurantService>());
+        Services.AddSingleton(Substitute.For<ILocalStorageService>());
+    }
+
+    protected void SetAuthenticatedUser(string username = "testuser", string role = "user")
+    {
+        AuthContext.SetAuthorized(username);
+        AuthContext.SetRoles(role);
+    }
+}
--- tests/Smakosz.ClientTests/GlobalUsings.cs
+++ tests/Smakosz.ClientTests/GlobalUsings.cs
@@ -1,1 +1,8 @@
 global using Xunit;
+global using Bunit;
+global using FluentAssertions;
+global using NSubstitute;
+global using Microsoft.Extensions.DependencyInjection;
+global using Smakosz.Client.Components;
+global using Smakosz.Client.Models;
+global using Smakosz.Client.Services;
