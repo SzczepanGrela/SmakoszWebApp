c159b99a1cddd17c9684adeddc5cc95abdf5a3a3
SzczepanGrela <szczepangrela@wp.pl>
2026-04-07 13:05:25 +0100

    added isremembered column to usersession because sliding window refresh needs to know which ttl to apply on rotation

---
+++ src/Smakosz.Infrastructure/Migrations/20260321180518_AddIsRememberMeToUserSession.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Smakosz.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsRememberMeToUserSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_remember_me",
+                table: "user_sessions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_remember_me",
+                table: "user_sessions");
+        }
+    }
+}
